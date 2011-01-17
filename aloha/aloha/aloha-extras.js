77d522ea21aadf55e82835525522b22c3cc4700b
evo42 <rene@evo42.net>
2011-01-17 16:42:24 +0100

    added alohaeditor as aloha -- move to alohaeditor later...

---
+++ aloha/aloha/aloha-extras.js
@@ -0,0 +1,17 @@
+/* maybe tmp file... we'll see... */
+
+
+(function(){
+	/* Check if base DIR is set through configuration otherwise set to default 
+	if (typeof GENTICS_Aloha_base == 'undefined') {
+			// use drupal js settings instead
+			// regex sites/all/libraries/alohaeditor ?
+			// now in alohaeditor.inc as drupal add js
+			GENTICS_Aloha_base = '/drupal7/drupal-7.0/sites/all/libraries/alohaeditor/aloha/'; 
+			//GENTICS_Aloha_base = '/sites/all/libraries/alohaeditor/WebContent/'; 
+	}
+	*/
+	
+// alert(GENTICS_Aloha_base);
+
+})();
